1bb5f447ad0f2041a28aec15a490be7ee8f8beda
leverglowh <leverglowh@gmail.com>
2019-03-15 00:31:51 +0100

    added some features

--- random/ViewController.swift
+++ random/ViewController.swift
@@ -14,11 +14,16 @@ class ViewController: NSViewController {
     @IBOutlet weak var ok: NSTextField!
     @IBOutlet weak var wordToKnow: NSTextField!
     @IBOutlet weak var loaded: NSTextField!
+    @IBOutlet weak var reverseStatus: NSTextField!
     
     
     var dataArray:[String] = []
     var path: String?
-
+    var leftVal: String = ""
+    var rightVal: String = ""
+    var left_array:[String] = []
+    var right_array:[String] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,31 +59,63 @@ class ViewController: NSViewController {
             ok.stringValue = "not ok"
         }
         
-        }
+    }
         
     
     @IBAction func loadFile(_ sender: NSButton) {
         if (path != nil) {
             let text = try! String(contentsOfFile:path!, encoding: String.Encoding.utf8)
             dataArray = text.components(separatedBy: "\n")
-            loaded.stringValue = "ok."
+            let splitArray = dataArray.split()
+            left_array = splitArray.left
+            right_array = splitArray.right
+            loaded.stringValue = "ok"
         } else {
-            loaded.stringValue = "errore."
+            loaded.stringValue = "errore"
         }
     }
     
     @IBAction func tryMe(_ sender: Any) {
-        let i = Int(arc4random_uniform(_:UInt32(dataArray.count)))
-        let randomVal = dataArray[i]
-        dataArray.remove(at: i)
-        if dataArray.count==0 {
+        
+        if left_array.count==0 {
             wordToKnow.stringValue = "Finitooo"
+        } else{
+            
+            let i = Int(arc4random_uniform(_:UInt32(left_array.count)))
+            leftVal = left_array[i]
+            rightVal = right_array[i]
+        
+            wordToKnow.stringValue = leftVal
+            left_array.remove(at: i)
+            right_array.remove(at: i)
+            
         }
-        else{
-            wordToKnow.stringValue = randomVal
-    }
+        
+        reverseStatus.stringValue = ""
+        
     }
     
+    
+    @IBAction func reverseRemove(_ sender: NSButton) {
+        left_array.append(leftVal)
+        right_array.append(rightVal)
+        reverseStatus.stringValue = "ok"
+    }
+    
+    @IBAction func giveUp(_ sender: NSButton) {
+        wordToKnow.stringValue = wordToKnow.stringValue + " " + rightVal
+        reverseStatus.stringValue = ""
+    }
+    
+}
 
+extension Array {
+    func split() -> (left: [Element], right: [Element]) {
+        let ct = self.count
+        let half = ct / 2
+        let leftSplit = self[0 ..< half]
+        let rightSplit = self[half+1 ..< ct]
+        return (left: Array(leftSplit), right: Array(rightSplit))
+    }
 }
 
